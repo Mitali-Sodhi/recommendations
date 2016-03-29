adbd1ed2870ae08e128dc5f2c72a039f5181b95c
Mitali-Sodhi <mitalisodhi.ec@gmail.com>
2016-03-30 00:16:21 +0530

    first commit

---
+++ recommendations.py
@@ -0,0 +1,8 @@
+critics = {'Lisa Rose': {'Lady in the water':2.5, 'Snakes on a plane' : 3.5, 'Just my luck' : 3.0, 'Superman returns' : 3.5, 'You, Me and Dupree' : 2.5, 'The Night Listener' : 3.0},
+'Gene Seymor' : {'Lady in the water' : 3.0, 'Snakes on a plane' : 3.5, 'Just my luck' : 1.5, 'Superman returns' : 5.0, 'The Night Listener' : 3.0, 'You, Me and Dupree' : 3.5},
+'Michael Plillips' : {'Lady in the water' : 2.5, 'Snakes on a plane' : 3.0, 'Superman returns' : 3.5, 'The Night Listener' : 4.0},
+'Claudia Puig' : {'Snakes on a plane' : 3.5, 'Just my luck' : 3.0, 'The Night Listener' : 4.5, 'Superman returns' : 4.0, 'You, Me and Dupree' : 2.5},
+'Mick LaSalle' : {'Lady in the water' : 3.0, 'Snakes on a plane' : 4.0, 'Just my luck' : 2.0, 'Superman returns' : 3.0, 'The Night Listener' : 3.0, 'You, Me and Dupree' : 2.0},
+'Jack Matthews' : {'Lady in the water' : 3.0, 'Snakes on a plane' : 4.0, 'The Night Listener' : 3.0, 'Superman returns' : 5.0, 'You, Me and Dupree' : 3.5},
+'Toby' : {'Snakes on a plane' : 4.5, 'You Me and Dupree' : 1.0, 'Superman returns' : 4.0}
+}
