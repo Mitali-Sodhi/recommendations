2f16599a6b409923def920c9f4d00f293a93b66b
Mitali-Sodhi <mitalisodhi.ec@gmail.com>
2016-04-18 23:02:04 +0530

    pearson distance included

--- recommendations.py
+++ recommendations.py
@@ -19,3 +19,30 @@ def sim_distance(prefs, person1, person2):
 
 	sum_of_squares = sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
 	return 1/(1+sum_of_squares)
+
+def sim_pearson(prefs, person1, person2):
+	si = {}
+	for item in prefs[person1]:
+		if item in prefs[person2]:
+			si[item] = 1
+	
+	n = len(si)
+	if n == 0:
+		return 0
+
+	sum1 = sum([prefs[person1][item] for item in si])
+	sum2 = sum([prefs[person2][item] for item in si])
+
+	sum1Sq = sum([pow(prefs[person1][item],2) for item in si])
+	sum2Sq = sum([pow(prefs[person2][item],2) for item in si])
+
+	pSum = sum([prefs[person1][item] * prefs[person2][item] for item in si])
+
+	num = pSum-(sum1*sum2/n)
+	den = sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+	if den == 0:
+		return 0
+	r=num/den
+	return r
+
+
